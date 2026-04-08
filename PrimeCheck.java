d421366da14dd226af3b3066dbc42f7b83d6a3f5
Bhushan Sonawane <bhushan.sonawane.tech@gmail.com>
2026-04-08 23:43:44 +0530

    Update PrimeCheck.java

--- PrimeCheck.java
+++ PrimeCheck.java
@@ -8,3 +8,18 @@ public class PrimeCheck {
             if (n % i == 0) return false;
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (isPrime(n)) {
+            System.out.println(n + " is a prime number");
+        } else {
+            System.out.println(n + " is not a prime number");
+        }
+        sc.close();
+    }
+}
