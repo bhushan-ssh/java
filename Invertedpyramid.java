53a2e94a4e810ab48087f6c6fdcf751722c058f6
Bhushan Sonawane <bhushan.sonawane.tech@gmail.com>
2026-04-20 18:56:03 +0530

    Okay

--- Invertedpyramid.java
+++ Invertedpyramid.java
@@ -8,6 +8,7 @@ public class Invertedpyramid {
             for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
+            
             System.out.println();
         }
     }
