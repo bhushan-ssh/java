b01945d2ff20f73f2844359abe33494b80f49515
Bhushan Sonawane <bhushan.sonawane.tech@gmail.com>
2026-04-22 20:14:49 +0530

    Okay

---
+++ Fibonacci.java
@@ -0,0 +1,12 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10, a = 0, b = 1;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
