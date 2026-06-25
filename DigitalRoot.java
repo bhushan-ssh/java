47d85cd640ec0d61771d85decfe5ef3c1a6d8145
bhushan-ssh <bhushan.sonawane.tech@gmail.com>
2026-06-25 23:19:16 +0530

    Updated

---
+++ DigitalRoot.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DigitalRoot {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        while(n >= 10) {
+            int sum = 0;
+
+            while(n > 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+
+            n = sum;
+        }
+
+        System.out.println("Digital Root = " + n);
+
+        sc.close();
+    }
+}
