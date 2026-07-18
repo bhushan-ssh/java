90081841ab8efbc448e185dff5b4b50008fce5d5
bhushan-ssh <bhushan.sonawane.tech@gmail.com>
2026-07-18 18:59:18 +0530

    Updated

---
+++ ElementFrequency.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ElementFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        boolean[] visited = new boolean[n];
+
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            System.out.println(arr[i] + " -> " + count);
+        }
+
+        sc.close();
+    }
+}
